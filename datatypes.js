10b0c77ff649737ab37ca558f7ebde340533716b
hansenjl <jenn.leigh.hansen@gmail.com>
2020-08-05 08:59:39 -0700

    finish study group

--- datatypes.js
+++ datatypes.js
@@ -2,7 +2,9 @@
 
 //String
 
-
+"double quote"
+'singe quote'
+`back ticks for ${'interpolation'}`
 
 
 
@@ -26,8 +28,8 @@
 //NaN  ex: 'hello' * 3
 
 // parseInt / parseFloat
-
-
+parseInt("2")
+parseFloat("2.52")
 
 
 
@@ -39,9 +41,15 @@
 //Boolean
 
 // Falsey values
+"" // empty string
+false // false
+0 // zero
+undefined
+null
+NaN
 
 
-//Truthy values (everything else)
+// Truthy values (everything else)
 
 
 
@@ -50,9 +58,10 @@
 
 
 //-----------------------
-// undefined
+// Undefined
 
-
+let name
+console.log(name)
 
 
 
@@ -69,7 +78,7 @@ console.log(someVar) // => null
 // -------------------
 // Symbol
 
-
+// use them as keys when making objects
 
 
 
@@ -78,11 +87,31 @@ console.log(someVar) // => null
 
 
 // ----------------------------------------------------------------------
-// Non-Primitives
+// Non-Primitives - pass by reference
+
+let arr = [1,2,3,4]
+arr === [1,2,3,4]
+
+function multiply(col){
+    let i = 0
+    col.forEach(e => {
+        col[i] = e * 2
+        i += 1
+    })
+}
+
+function mult(num){
+    return num = num*2
+}
 
 // Object  (object literals)
 
+const person = {name: 'Fred', age: '20'}
+const school = {name: 'high school', students: [person]}
 
+person.age = 30
+person["name"] = "Freddy"
+person["height"] = "tall"
 
 
 
@@ -93,14 +122,23 @@ console.log(someVar) // => null
         console.log('Pancake technology is truly incredible. What a time to be alive!')
     }
     iLikePancakes
-    iLikePancakes()
+    iLikePancakes()  // CALLING THE FUNCTION
 
+    function doubleFunction(){
+        let greeting = "hi"
+        console.log("Parent function")
+        return function(){
+            console.log(greeting)
+        }
+    }
+
+    doubleFunction()()
 
 // Function Expression
 let arr = [1,2,3]
 let doSomething = function() { return true }
 let doSomethingElse = () => false
-arr.map(function namedExpression(n) { return n + 1 })
+arr.map(function(n) { return n + 1 })
 
 
 // Function declaration
@@ -122,3 +160,8 @@ console.log(doSomething)
 // ----------------
 // Array
 let arr = [1, 2, 3, "a", "b", "c", { hello: 'world' }]
+// .map
+// .reduce
+// .forEach
+// .split
+// MDN documentation for array methods
